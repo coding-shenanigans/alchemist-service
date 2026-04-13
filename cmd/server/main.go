7d7a88c108231d147f1f1fee240fdca2c715a7ca
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2026-04-12 23:15:00 -0700

    Create a custom cors config (#11)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
+	"github.com/coding-shenanigans/alchemist-service/internal/config"
 	"github.com/coding-shenanigans/alchemist-service/internal/handler"
 	"github.com/coding-shenanigans/alchemist-service/internal/middleware"
 )
 
 func main() {
 	router := gin.Default()
-	router.Use(cors.Default())
+	router.Use(cors.New(config.GetCorsConfig()))
 
 	publicRouterGroup := router.Group("/")
 	protectedRouterGroup := router.Group("/", middleware.AuthMiddleware())
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
+
+	"github.com/gin-contrib/cors"
 )
 
 const (
@@ -79,4 +82,16 @@ func GetEnvBool(key string) bool {
 	}
 
 	return value
+}
+
+// Creates a custom CORS config.
+func GetCorsConfig() cors.Config {
+	return cors.Config{
+		// TODO: Fetch origins from env or constants file.
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
 }
