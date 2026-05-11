c8e6f56f255ae7e6ff9e3c38e79d37d31d5d8516
coding-shenanigans <coding.shenanigans@gmail.com>
2026-05-10 22:55:20 -0700

    Store JWT id instead of the token

--- internal/model/session.go
+++ internal/model/session.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Session struct {
-	Id           int
-	UserId       int       `db:"user_id"`
-	RefreshToken string    `db:"refresh_token"`
-	CreatedAt    time.Time `db:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at"`
+	Id             int
+	UserId         int       `db:"user_id"`
+	RefreshTokenId string    `db:"refresh_token_id"`
+	CreatedAt      time.Time `db:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at"`
 }
